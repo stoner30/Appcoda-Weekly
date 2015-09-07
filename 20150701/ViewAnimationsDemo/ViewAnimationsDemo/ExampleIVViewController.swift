2d44da16a3372b181e0f37003d253fd257dc9f5f
Stoner Wang <stoner.wong@gmail.com>
2015-09-07 17:35:14 +0800

    20150701: 创建了项目的基本框架

---
+++ 20150701/ViewAnimationsDemo/ViewAnimationsDemo/ExampleIVViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ExampleIVViewController.swift
+//  ViewAnimationsDemo
+//
+//  Created by Stoner Wang on 15/9/7.
+//  Copyright © 2015年 Stoner.Wong. All rights reserved.
+//
+
+import UIKit
+
+class ExampleIVViewController: UITableViewController {
+
+    let tableItems = [
+        "Item 1", "Item 2", "Item 3", "Item 4", "Item 5",
+        "Item 6", "Item 7", "Item 8", "Item 9", "Item 10",
+        "Item 11", "Item 12", "Item 13", "Item 14", "Item 15"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableItems.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = tableItems[indexPath.row]
+        
+        return cell
+    }
+
+}
